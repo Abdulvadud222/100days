23ebf1c9708c9a7c670115f30cda8117131d679e
Abdulvadud21 <Abdulvadud_Akhmadkhanov@student.itpu.uz>
2024-12-10 10:27:05 +0500

    Add the real day1

--- day1.py
+++ day1.py
@@ -1,1 +1,67 @@
-print("hey")
+# a = input("a: ")
+# b = input("b: ")
+#
+# c = a #employing a third cup
+# a = b
+# b = c
+#
+# print(f"a is equal to {a}")
+# print(f"b is equal to {b}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
