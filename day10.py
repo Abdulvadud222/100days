16be1651fa8a134d15e8027c566dfa77ff6dcfad
Abdulvadud21 <Abdulvadud_Akhmadkhanov@student.itpu.uz>
2024-12-29 13:11:24 +0500

    Update it again

--- day10.py
+++ day10.py
@@ -39,53 +39,53 @@
 
 
 ###final project: calculator
-
-
-
-def add(n1, n2):
-    """Rturns the addition"""
-    return n1 + n2
-
-def subtract(n1, n2):
-    """Return the subtraction"""
-    return n1 - n2
-
-def multiply(n1, n2):
-    """Retrun multiplication"""
-    return n1 * n2
-
-def divide(n1, n2):
-    """Return Division"""
-    return n1 / n2
-
-operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide,
-}
-def calculator():
-    num1 = float(input("What's the first number: "))
-    for symbol in operations:
-        print(symbol)
-
-    should_continue = True
-
-    while should_continue:
-        choice = input("Pick an operation: ")
-        if choice not in ["+", "-", "*", "/"]:
-            print("Invalid operation symbol! Please pick from +, -, *, /.\n"
-                  "Exiting...")
-            break
-        num2 = float(input("What's the next number: "))
-        operation = operations[choice]
-        answer = operation(num1, num2)
-        print(f"{num1} {choice} {num2} = {answer}")
-        again = input(f"If you want to continue with {answer}, type y. Otherwise type n: ").lower()
-        if again in ["y"]:
-            should_continue = True
-            num1 = answer
-        else:
-            should_continue = False
-            calculator()
-calculator()        
+#
+#
+#
+# def add(n1, n2):
+#     """Returns the addition"""
+#     return n1 + n2
+#
+# def subtract(n1, n2):
+#     """Return the subtraction"""
+#     return n1 - n2
+#
+# def multiply(n1, n2):
+#     """Returns multiplication"""
+#     return n1 * n2
+#
+# def divide(n1, n2):
+#     """Return Division"""
+#     return n1 / n2
+#
+# operations = {
+#     "+": add,
+#     "-": subtract,
+#     "*": multiply,
+#     "/": divide,
+# }
+# def calculator():
+#     num1 = float(input("What's the first number: "))
+#     for symbol in operations:
+#         print(symbol)
+#
+#     should_continue = True
+#
+#     while should_continue:
+#         choice = input("Pick an operation: ")
+#         if choice not in ["+", "-", "*", "/"]:
+#             print("Invalid operation symbol! Please pick from +, -, *, /.\n"
+#                   "Exiting...")
+#             break
+#         num2 = float(input("What's the next number: "))
+#         operation = operations[choice]
+#         answer = operation(num1, num2)
+#         print(f"{num1} {choice} {num2} = {answer}")
+#         again = input(f"If you want to continue with {answer}, type y. Otherwise type n: ").lower()
+#         if again in ["y"]:
+#             should_continue = True
+#             num1 = answer
+#         else:
+#             should_continue = False
+#             calculator()
+# calculator()
