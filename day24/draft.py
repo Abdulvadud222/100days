c037b0f98af8b1a14449b2154557a349f09d41ed
Abdulvadud21 <Abdulvadud_Akhmadkhanov@student.itpu.uz>
2024-12-31 17:18:35 +0500

    Update day20

---
+++ day24/draft.py
@@ -0,0 +1,11 @@
+# with open("my_file.txt") as file: ###this feature allows the user to open files without having to close it. Now the txt
+#     ###file is equal to the variable "file"
+#     contents = file.read() ### this turns everything in file into string and assigns them to the variable contents
+#     print(contents)
+
+with open("my_file.txt", mode="a") as file: ###if mode is "w", we overwrite the text. if it is "a", we append
+    file.write("New text")
+    file.write(" has been added!")
+
+
+
